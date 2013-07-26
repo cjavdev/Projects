6150ebb7cb58e1531959a0a9732bf6ac6b3c2750
CJ Avilla <cjavilla@gmail.com>
2013-07-25 21:20:59 -0700

    Adding jasmine specs to test javascript implementations of datastructures

---
+++ DataStructures/public/javascripts/Trie.js
@@ -0,0 +1,44 @@
+var Trie = function() {
+  
+};
+
+Trie.prototype.addWord = function(word) {
+  console.log(word);
+};
+
+Trie.prototype.containsPrefix = function(prefix) {
+  console.log(prefix);
+};
+
+Trie.prototype.containsWord = function(word) {
+
+};
+
+Trie.prototype.getWord = function(word) {
+
+};
+
+Trie.prototype.getPrefix = function(prefix) {
+
+};
+
+var Node = function(ch, value) {
+  this.ch = ch;
+  this.value = value;
+};
+
+Node.prototype.addChild = function(node) {
+  
+};
+
+Node.prototype.containsChildValue = function(c) {
+
+};
+
+Node.prototype.getChild = function(c) {
+
+};
+
+Node.prototype.isWord = function() {
+
+};
