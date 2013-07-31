ffcfdeccaeb5a1ec1c43a3396ca482eeda00c53d
CJ Avilla <cjavilla@gmail.com>
2013-07-30 17:37:42 -0700

    Adding graph

--- DataStructures/lib/graph.rb
+++ DataStructures/lib/graph.rb
@@ -3,13 +3,21 @@ require 'debugger'
 class Node
   attr_accessor :neighbors, :value
 
-  def initialize value
+  def initialize(value)
     @value = value
     @neighbors = {}
   end
 
   def add_neighbor(neighbor, weight = 1)
     @neighbors[neighbor.value] = [neighbor, weight]
+  end
+
+  def adjacent?(neighbor)
+    @neighbors.keys.include?(neighbor.value)
+  end
+
+  def adjacent_val?(neighbor)
+    @neighbors.keys.include?(neighbor)
   end
 end
 
@@ -20,7 +28,7 @@ class Graph
     @nodes = {}
   end
 
-  def add_node_by_val(val)
+  def add_node_by_value(val)
     @nodes[val] = Node.new(val)
   end
 
@@ -34,5 +42,17 @@ class Graph
 
   def add_edge(from, to, weight = 1)
     @nodes[from.value].add_neighbor(to, weight)
+  end
+
+  def adjacent?(from, to)
+    @nodes[from.value].adjacent?(to)
+  end
+
+  def adjacent_by_value(from, to)
+    @nodes[from].adjacent?(to)
+  end
+
+  def edge_value(from, to)
+    @nodes[from.value].neighbors[to.value][1] if @nodes[from.value].neighbors[to.value]
   end
 end
