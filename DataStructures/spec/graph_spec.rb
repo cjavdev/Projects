7ece284b2715adb7132106e3954ed1aafa4e9730
CJ Avilla <cjavilla@gmail.com>
2013-07-30 17:37:56 -0700

    Adding graph

--- DataStructures/spec/graph_spec.rb
+++ DataStructures/spec/graph_spec.rb
@@ -12,6 +12,7 @@ describe Graph do
   subject { Graph.new }
   let(:n1) { Node.new(1) }
   let(:n2) { Node.new(2) }
+  let(:n3) { Node.new(3) }
 
   it "#add_node adds a node" do
     subject.add_node(n1)
@@ -25,5 +26,23 @@ describe Graph do
     subject.nodes[n1.value].value.should == 1
     subject.nodes[n1.value].neighbors.count.should == 1
     subject.nodes[n2.value].neighbors.count.should == 0
+  end
+
+  it "#adjacent? checks if a node is adjacent to another" do
+    subject.add_node(n1)
+    subject.add_node(n2)
+    subject.add_node(n3)
+    subject.add_edge(n1, n2)
+    subject.adjacent?(n1, n2).should == true
+    subject.adjacent?(n1, n3).should == false
+  end
+
+  it "#edge_value gets edge value between n1, and n2" do
+    subject.add_node(n1)
+    subject.add_node(n2)
+    subject.add_node(n3)
+    subject.add_edge(n1, n2, 4)
+    subject.edge_value(n1, n2).should == 4
+    subject.edge_value(n1, n3).should == nil
   end
 end
