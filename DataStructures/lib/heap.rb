8abb159a1a1a840ccc4c9aa912b8e765856895a1
CJ Avilla <cjavilla@gmail.com>
2013-08-01 17:19:05 -0700

    Adding heap again

--- DataStructures/lib/heap.rb
+++ DataStructures/lib/heap.rb
@@ -1,57 +1,114 @@
-class Node
-  attr_accessor :value, :left, :right, :parent
+require 'debugger'
 
+class HeapNode
+  attr_accessor :value, :left, :right, :parent
+  include Comparable
+  
   def initialize value
     @value = value
   end
   
   def <=>(other)
-    other.value <=> @value  
+    @value  <=> other.value  
   end
 end
 
 class Heap
-  attr_accessor :max, :count
-  def initialize
-    @nodes = []
+  attr_accessor :root, :compare_proc
+  
+  def initialize &compare_proc
     @root = nil
+    @compare_proc = compare_proc
   end
   
   def insert node
+    if @root.nil?
+      @root = node
+      return
+    end
+      
+    f = @root
+    f = f.left || f.right until f.left.nil? || f.right.nil?
+    if f.left
+      f.right = node
+    else
+      f.left = node
+    end
+    node.parent = f
     
+    heap_up node
+  end
+  
+  def heap_up node
+    sorted = false
+    until sorted
+      sorted = true
+      if node.parent && @compare_proc.call(node, node.parent) == 1
+        node.value, node.parent.value = node.parent.value, node.value
+        node = node.parent
+        sorted = false
+      elsif node.parent.nil?
+        @root = node
+      end
+    end
+    
+    if (@root.left  && @compare_proc.call(@root.left, @root) == 1) || 
+       (@root.right && @compare_proc.call(@root.right, @root) == 1)
+      heap_up @root
+    end
+  end
+  
+  def heap_down node
+    sorted = false
+    until sorted
+      sorted = true
+      if node.left &&  @compare_proc.call(node.left, node) == 1
+        node.left.value, node.value = node.value, node.left.value
+        node = node.left
+        sorted = false
+      elsif node.right &&  @compare_proc.call(node.right, node) == 1
+        node.right.value, node.value = node.value, node.right.value
+        node = node.right
+        sorted = false
+      end
+    end
+    
+    if (@root.left  && @compare_proc.call(@root.left, @root) == 1) || 
+       (@root.right && @compare_proc.call(@root.right, @root) == 1)
+      heap_down @root
+    end
   end
 
   def remove
+    result = @root.dup
     
-  end
- 
-  def swap(big, small)
-    if small.parent.nil? #smaller was root
-      if small.left == big
-        small.left = big.left
-        small.right = big.right
-        big.left = small
-        big.right = nil
-      elsif small.right == big
-         
-      end
+    f = @root
+    f = f.left || f.right until f.left.nil? && f.right.nil?
+    @root.value, f.value = f.value, @root.value
+    if f.parent && f.parent.left == f
+      f.parent.left = nil
+    elsif f.parent && f.parent.right == f
+      f.parent.right = nil
     else
-      if small.parent.left == small
-        small.parent.left = big
-      elsif small.parent.right == small
-        small.parent.right = big
+      if @root.left.nil? && @root.right.nil? && f == @root
+        @root = nil 
       end
+      f = nil
     end
+    
+    heap_down @root if @root
+    result
   end
+end
 
-  def heapify
-    @finder = @max
-    temp_nodes = @nodes.dup
-    until temp_nodes.empty
-      current = temp_nodes.pop
-      if current > current.parent
-        swap(current, current.parent)
-      end
-    end
+class MaxHeap < Heap
+  def initialize
+    super { |x, y| x <=> y }
+  end
+end
+
+class MinHeap < Heap
+  def initialize
+    super { |x, y| y <=> x }
   end
 end
---
+++ DataStructures/spec/heap_spec.rb
@@ -0,0 +1,135 @@
+require 'rspec'
+require 'heap'
+
+describe Heap do
+  subject { Heap.new {|x,y| x<=>y } }
+  let(:n1) { HeapNode.new(1) }
+  let(:n2) { HeapNode.new(2) }
+  let(:n3) { HeapNode.new(3) }
+  let(:n4) { HeapNode.new(4) }
+  let(:n5) { HeapNode.new(5) }
+  let(:n6) { HeapNode.new(6) }
+  
+  describe "#insert" do
+    it "can insert a single item" do
+      subject.insert n1
+      subject.root.should == n1
+    end
+  
+    it "can insert multiple items correctly" do
+      subject.insert n1
+      subject.insert n2
+      subject.root.value.should == 2
+      
+      subject.insert n6
+      subject.insert n4
+      subject.insert n5
+      subject.insert n3
+      subject.root.value.should == 6
+    end
+  end
+  
+  describe "#remove" do
+    it "can properly remove the biggest item" do
+      subject.insert n1
+      subject.insert n2
+      subject.remove.value.should == 2
+      subject.remove.value.should == 1
+      subject.insert n3
+      subject.insert n6
+      subject.insert n5
+      subject.insert n4
+      subject.remove.value.should == 6
+      subject.remove.value.should == 5
+      subject.remove.value.should == 4
+      subject.remove.value.should == 3
+    end
+  end
+end
+
+describe MaxHeap do
+  subject { MaxHeap.new }
+  let(:n1) { HeapNode.new(1) }
+  let(:n2) { HeapNode.new(2) }
+  let(:n3) { HeapNode.new(3) }
+  let(:n4) { HeapNode.new(4) }
+  let(:n5) { HeapNode.new(5) }
+  let(:n6) { HeapNode.new(6) }
+  
+  describe "#insert" do
+    it "can insert a single item" do
+      subject.insert n1
+      subject.root.should == n1
+    end
+  
+    it "can insert multiple items correctly" do
+      subject.insert n1
+      subject.insert n2
+      subject.root.value.should == 2
+      
+      subject.insert n6
+      subject.insert n4
+      subject.insert n5
+      subject.insert n3
+      subject.root.value.should == 6
+    end
+  end
+  
+  describe "#remove" do
+    it "can properly remove the biggest item" do
+      subject.insert n1
+      subject.insert n2
+      debugger
+      subject.remove.value.should == 2
+      subject.remove.value.should == 1
+      subject.insert n3
+      subject.insert n6
+      subject.insert n5
+      subject.insert n4
+      subject.remove.value.should == 6
+      subject.remove.value.should == 5
+      subject.remove.value.should == 4
+    end
+  end
+end
+
+describe MinHeap do
+  subject { MinHeap.new }
+  let(:n1) { HeapNode.new(1) }
+  let(:n2) { HeapNode.new(2) }
+  let(:n3) { HeapNode.new(3) }
+  let(:n4) { HeapNode.new(4) }
+  let(:n5) { HeapNode.new(5) }
+  let(:n6) { HeapNode.new(6) }
+  
+  describe "#insert" do
+    it "can insert multiple items correctly" do
+      subject.insert n1
+      subject.insert n2
+      subject.root.value.should == 1
+    
+      subject.insert n6
+      subject.insert n4
+      subject.insert n5
+      subject.insert n3
+      subject.root.value.should == 1
+      subject.insert HeapNode.new(0)
+      subject.root.value.should == 0
+    end
+  end
+
+  describe "#remove" do
+    it "can properly remove the smallest item" do
+      subject.insert n1
+      subject.insert n2
+      subject.remove.value.should == 1
+      subject.remove.value.should == 2
+      subject.insert n3
+      subject.insert n6
+      subject.insert n5
+      subject.insert n4
+      subject.remove.value.should == 3
+      subject.remove.value.should == 4
+    end
+  end
+end
