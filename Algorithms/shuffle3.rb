be989529ba52732a4ff5405f7e38370722bc3ac8
CJ Avilla <cjavilla@gmail.com>
2013-09-17 12:13:10 -0700

    add new shuffle

---
+++ Algorithms/shuffle3.rb
@@ -0,0 +1,33 @@
+require 'rgl/adjacency'
+
+
+g = RGL::DirectedAdjacencyGraph.new
+
+p g
+
+
+students = [:oddalot,
+            :persamina,
+            :djquan,
+            :RussHR,
+            :jsrosen3,
+            # :amtrakcn,
+            # "giant-squid",
+            # :jsinghdreams,
+            # :atseng3,
+            # :justalisteningman,
+            # :Zehhu,
+            # :bwarkee,
+            # :aarnwri,
+            # :guillewu,
+            # :dyang12,
+            # :izhou,
+            # :vpark,
+            :tnd23 ]
+
+students.combination(2).each do |pair|
+  g.add_edge(pair.first, pair.last)
+end
+p g
+
+
